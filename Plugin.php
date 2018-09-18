2c8050255dcba872ae100ade201669d3a104c3e2
CRYDEsigN <Cryden_da@mail.ru>
2018-09-18 12:30:32 +0300

    add twig filter ru_date

--- Plugin.php
+++ Plugin.php
@@ -1,14 +1,33 @@
 <?php namespace fw\Backend;
 
 use System\Classes\PluginBase;
+use Carbon\Carbon;
 
 class Plugin extends PluginBase
 {
-    public function registerComponents()
-    {
-    }
+  public function registerComponents()
+  {
+  }
 
-    public function registerSettings()
-    {
-    }
+  public function registerSettings()
+  {
+  }
+
+  public function registerMarkupTags()
+  {
+    return [
+      'filters' => [
+        'ru_date' => [$this, 'ruDate'],
+      ],
+    ];
+  }
+
+  public function ruDate($text)
+  {
+    $months = [1 => 'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+    $date = Carbon::createFromFormat('Y-m-d', $text);
+    $key = $date->format('n');
+    return $date->format('d ' . $months[$key] . ' Y');
+  }
+
 }
