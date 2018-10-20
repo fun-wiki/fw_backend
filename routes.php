051678813e2da794cb2a7d05b2d0734f972784ea
Cryden <cryden_da@mail.ru>
2018-10-20 14:04:04 +0300

    update sitemap.xml

--- routes.php
+++ routes.php
@@ -1,12 +1,13 @@
 <?php
 
 use \Fw\Backend\Models\Person;
+use \Fw\Backend\Models\Universe;
 
 Route::get('sitemap.xml', function()
     {
         $persons = Person::all();
-        //$genres = Genre::all();
-        return Response::view('fw.backend::sitemap', ['persons' => $persons,])->header('Content-Type', 'text/xml');
+        $universes = Universe::all();
+        return Response::view('fw.backend::sitemap', ['persons' => $persons, 'universes' => $universes])->header('Content-Type', 'text/xml');
     }
 );
 ?>
