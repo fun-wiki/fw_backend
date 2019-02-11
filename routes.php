061a098b9a1dc0d61968bbb7a7b9a2c1162936d4
CRYDEsigN <Cryden_da@mail.ru>
2019-02-11 09:35:44 +0300

    min update add bookeditions in sitemap

--- routes.php
+++ routes.php
@@ -7,7 +7,12 @@ Route::get('sitemap.xml', function()
     {
         $persons = Person::all();
         $universes = Universe::all();
-        return Response::view('fw.backend::sitemap', ['persons' => $persons, 'universes' => $universes])->header('Content-Type', 'text/xml');
+        $bookEdition = \fw\Backend\Models\BookEdition::all();
+        return Response::view('fw.backend::sitemap', [
+            'persons' => $persons, 
+            'universes' => $universes,
+            'book_edition' => $bookEdition
+        ])->header('Content-Type', 'text/xml');
     }
 );
 ?>
