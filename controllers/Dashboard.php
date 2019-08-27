9b6774dc1549c42383baadfdf4e28d16c8b03bfc
CRYDEsigN <Cryden_da@mail.ru>
2019-08-27 11:54:39 +0300

    add dashboard

--- controllers/Dashboard.php
+++ controllers/Dashboard.php
@@ -11,6 +11,17 @@ class Dashboard extends Controller
     public function __construct()
     {
         parent::__construct();
-        BackendMenu::setContext('fw.Backend', 'fw-menu', 'work');
+        BackendMenu::setContext('fw.Backend', 'fw-menu', 'dashboard');
+    }
+
+    public function index() 
+    {
+        $this->pageTitle = 'Панель управления';
+        $this->AddCss('/plugins/fw/backend/assets/css/style.css');
+        //dump($this);
+        $this->vars['content_published'] = \fw\Backend\Models\Content::where('status', 'published')->count();
+        $this->vars['content_draft'] = \fw\Backend\Models\Content::where('status', 'draft')->count();
+        $this->vars['content_not_status'] = \fw\Backend\Models\Content::where('status', null)->count();
+        $this->vars['content_deleted'] = \fw\Backend\Models\Content::where('status', 'deleted')->count();
     }
 }
