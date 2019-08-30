3be34fde4781ae44a376ddafd202bbc9aa74a73e
Cryden <cryden_da@mail.ru>
2019-08-30 21:47:17 +0300

    fix person model

--- models/Person.php
+++ models/Person.php
@@ -22,9 +22,9 @@ class Person extends Model
     public $belongsToMany = [
         'persons' => ['fw\Backend\Models\Person', 'table' => 'fw_backend_relation_persons_pseudos', 'key' => 'pseudo_id', 'otherKey' => 'person_id',],
         'pseudos' => ['fw\Backend\Models\Person', 'table' => 'fw_backend_relation_persons_pseudos', 'key' => 'person_id', 'otherKey' => 'pseudo_id',],
-        'genres' => ['fw\Backend\Models\Genre', 'table' => 'fw_backend_persons_genres'],
-        'universes' => ['fw\Backend\Models\Universe', 'table' => 'fw_backend_universes_persons'],
-        'personroles' => ['fw\Backend\Models\PersonRole', 'table' => 'fw_backend_persons_persons_roles']
+        'genres' => ['fw\Backend\Models\Genre', 'table' => 'fw_backend_relation_persons_genres'],
+        'universes' => ['fw\Backend\Models\Universe', 'table' => 'fw_backend_relation_universes_persons'],
+        'personroles' => ['fw\Backend\Models\PersonRole', 'table' => 'fw_backend_persons_persons_roles'] // исправить
     ];
 
     public $morphOne = [
