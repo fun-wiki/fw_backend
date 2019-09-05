4fd6088a55f46895428026f18fe82ad9f171d28d
CRYDEsigN <Cryden_da@mail.ru>
2019-09-05 10:22:40 +0300

    add RSS feed

--- Routes.php
+++ Routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use \Fw\Backend\Models\Content;
+use Carbon\Carbon;
 
 Route::get(
     'sitemap.xml',
@@ -8,6 +9,18 @@ Route::get(
         $content = Content::where(['status' => 'published'])->get();
         return Response::view('fw.backend::sitemap', [
             'content' => $content
+        ])->header('Content-Type', 'text/xml');
+    }
+);
+
+Route::get(
+    'rss.xml',
+    function () {
+        $content = Content::where(['status' => 'published'])->orderBy('published_at' , 'desc')->get();
+        $lastBuildDate = Carbon::now();
+        return Response::view('fw.backend::rss', [
+            'content' => $content,
+            'lastBuildDate' => $lastBuildDate
         ])->header('Content-Type', 'text/xml');
     }
 );
